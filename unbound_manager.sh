164fd5230acda5981f2a870effd10bc4c84bd515
MartineauUK <MartineauUK@users.noreply.github.com>
2020-05-22 12:28:35 +0100

    Update unbound_manager.sh

--- unbound_manager.sh
+++ unbound_manager.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # shellcheck disable=SC2086,SC2068,SC1087,SC2039,SC2155,SC2124,SC2027,SC2046
-#============================================================================================ © 2019-2020 Martineau v3.16
+#============================================================================================ © 2019-2020 Martineau v3.16b2
 #  Install 'unbound - Recursive,validating and caching DNS resolver' package from Entware on Asuswrt-Merlin firmware.
 #
 # Usage:    unbound_manager    ['help'|'-h'] | [ [debug] ['nochk'] ['advanced'] ['install'] ['recovery' | 'restart' ['reload config='[config_file] ]] ]
